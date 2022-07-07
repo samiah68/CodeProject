6f86947a739027857f9a1460285bd3a513be91e3
samiah68 <108763519+samiah68@users.noreply.github.com>
2022-07-07 15:13:19 -0500

    Add files via upload

---
+++ mouseTagGame.js
@@ -0,0 +1,67 @@
+//variables for the position and speed of game project
+let xPos;
+let yPos;
+
+
+let xSpeed = 2;
+let ySpeed = 2;
+
+//game project
+let kitten
+
+//calculate mouse distance from babykitten
+let mouseDist
+
+//score variable
+let score = 0
+
+function preload(){
+  kitten = loadImage('babykitten.jpg')
+}
+function setup(){
+  createCanvas(windowWidth, windowHeight)
+
+
+  xPos = random(15, windowWidth - 15)
+  yPos = random(15, windowHeight - 15)
+
+  textAlign(CENTER)
+}
+
+function draw(){
+  background(0)
+
+  fill(255)
+  textSize(40)
+  text("Your score is " + score, windowWidth/2, 50)
+
+  mouseDist = dist(mouseX, mouseY, xPos, yPos)
+
+  print(mouseDist)
+
+  image(kitten, xPos, yPos, 30, 30);
+  xPos = xPos + xSpeed
+  yPos = yPos + ySpeed
+
+  if(xPos >= windowWidth - 15 || xPos <= 0){
+    xSpeed = xSpeed * -1;
+  }
+
+  if(yPos >= windowHeight - 15 || yPos <= 0){
+    ySpeed = ySpeed * -1;
+  }
+
+  if(mouseDist <= 15){
+    xPos = random(15, windowWidth - 15)
+    yPos = random(15, windowHeight - 15)
+    score++
+    xSpeed = xSpeed * 1.2
+    ySpeed = ySpeed * 1.2
+  }
+
+}
+
+//check to see if the window size was changed
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
+}
