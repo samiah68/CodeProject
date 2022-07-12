e46e8431b2a4d26fda6e365c2df0cc7bd29b13f8
samiah68 <108763519+samiah68@users.noreply.github.com>
2022-07-12 12:21:04 -0500

    Add files via upload

---
+++ circleGame.js
@@ -0,0 +1,18 @@
+let bg;
+
+
+function preload(){
+  bg = loadImage('20477459.jpg')
+}
+function setup(){
+
+  bg = loadImage('20477459.jpg')
+  createCanvas(1366, 648)
+}
+
+function draw(){
+  background(bg);
+  circle(30, 30, 20);
+
+
+}
